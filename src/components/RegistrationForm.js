2b574a498e51c3917345172790bad6daf4a96c9f
Sachin Bhandari <sachinb443@gmail.com>
2018-01-20 11:54:10 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,8 +4,8 @@ class RegistrationForm extends React.Component{
   render(){
     return (
       <form>
-        <input type="text">
-        <input type="password">
+        <input type="text"></input>
+        <input type="password"></input>
         <button type="submit"></button>
       </form>
     );
--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,7 +2,7 @@ import FillerText from './FillerText';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const title= React.createElement('p',{},"The world's coolest webpage.'")
+const title= React.createElement('p',{},"The world's coolest webpage.'");
 
 ReactDOM.render(
   title,

1f8e841aabf32c36da23679db4a0ec3f756a5187
Sachin Bhandari <sachinb443@gmail.com>
2018-01-20 11:49:41 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -2,7 +2,8 @@ import FillerText from './FillerText';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-const title= React.createElement('p',{},'The world's coolest webpage.');
+const title= React.createElement('p',{},"The world's coolest webpage.'")
+
 ReactDOM.render(
   title,
   <FillerText />,
